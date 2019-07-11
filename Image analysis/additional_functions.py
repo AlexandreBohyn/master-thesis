169cd3fe13182b8e96badf43ae0b3677903cecc2
Alexandre <alexandre.bohyn@gmail.com>
2019-07-11 12:12:03 +0200

    Added percentage area function

--- Image analysis/additional_functions.py
+++ Image analysis/additional_functions.py
@@ -2,6 +2,9 @@
 """
 File with additional functions for the convex hull area of the pictures
 """
+import cv2
+import numpy as np
+
 def PolyArea2D(pts):
     """Compute the area of a 2D polygon formed by a set of points
 
@@ -35,54 +38,28 @@ def convex_hull_area(thresh_img,thresh_val):
         hull_list[i] = cv2.convexHull(contours[i]);
         hull_area[i] = cv2.contourArea(hull_list[i]);
     return sum(hull_area);
- 
 
+def area_percentage(img_path,thresh_val,flipBool,BoundingArray):
+    """Compute the area of the root system as the percentage of black pixels
+    in the whole image after binarizing it. Crops the image to a specified
+    rectangle and flips it if needed.
 
-import cv2
-import numpy as np
+    Parameters:
+    img_path (str): Path of the image
+    thresh_val (int): Threshold value for the binarization of the image
+    flipBool (bool): Boolean specifying whether the image should be flipped 
+    BoundingArray: Array with the X, Y, Width, Length of the bounding box for
+        the cropping
 
-img = cv2.imread('wallet.jpg',)
+    Returns:
+    float: percentage of total area occupied by the root system
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
- 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_, thresh = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY_INV)
-thresh = cv2.GaussianBlur(thresh, (3,3), 0)
-
-cv2.imshow('image',thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img, contours, 0, (0,255,0), 2)
-
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# create hull array for convex hull points
-hull = []
- 
-# calculate points for each contour
-for i in range(len(contours)):
-    # creating convex hull object for each contour
-    hull.append(cv2.convexHull(contours[i], False))
-    
-# create an empty black image
-drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
- 
-# draw contours and hull points
-for i in range(len(contours)):
-    color_contours = (0, 255, 0) # green - color for contours
-    color = (255, 0, 0) # blue - color for convex hull
-    # draw ith contour
-    cv2.drawContours(drawing, contours, i, color_contours, 1, 8, hierarchy)
-    # draw ith convex hull object
-    cv2.drawContours(drawing, hull, i, color, 1, 8)
-
-cv2.imshow('image',drawing)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+   """
+    img = cv2.imread(img_path,0);
+    if(flipBool):
+        img = cv2.flip(img, 1 );
+    img = img[BoundingArray[0]:BoundingArray[0]+BoundingArray[2],\
+              BoundingArray[1]:BoundingArray[1]+BoundingArray[3]];
+    _,thresh = cv2.threshold(img,thresh_val,255, cv2.THRESH_BINARY_INV);
+    perc = cv2.countNonZero(thresh)/(np.size(img))*100;
+    return perc;
