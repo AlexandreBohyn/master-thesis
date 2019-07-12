e744f2b6453a71877845ca18b1a79c79f25600a0
Alexandre <alexandre.bohyn@gmail.com>
2019-07-12 10:06:31 +0200

    Changes to script to run every photo analysis

--- Image analysis/image_analysis.py
+++ Image analysis/image_analysis.py
@@ -13,6 +13,7 @@ Image analysis code.
 import pandas as pd
 import numpy as np
 import cv2
+import glob
 
 #%% Create the functions
 def area_percentage(img_path,thresh_val,flipBool,BoundingArray):
@@ -34,8 +35,7 @@ def area_percentage(img_path,thresh_val,flipBool,BoundingArray):
     img = cv2.imread(img_path,0);
     if(flipBool):
         img = cv2.flip(img, 1 );
-    img = img[BoundingArray[0]:BoundingArray[0]+BoundingArray[2],\
-              BoundingArray[1]:BoundingArray[1]+BoundingArray[3]];
+    img = img[BoundingArray[0]:BoundingArray[0]+BoundingArray[2],BoundingArray[1]:BoundingArray[1]+BoundingArray[3]];
     _,thresh = cv2.threshold(img,thresh_val,255, cv2.THRESH_BINARY_INV);
     perc = cv2.countNonZero(thresh)/(np.size(img))*100;
     return perc;
@@ -44,26 +44,30 @@ def area_percentage(img_path,thresh_val,flipBool,BoundingArray):
 #%% Read the data and apply the function
 
 #Read the data and extract the last 990 photos
-data = pd.read_csv('EntryFile.txt', sep = '\t', header = 0);
-data_filtered = data.query('Folder == "13-03-19"');
-data_filtered.index = np.arange(990);
+data_filtered = pd.read_csv('EntryFile.txt', sep = '\t', header = 0);
+# data_filtered = data.query('Folder == "13-03-19"'); # Uncomment this line to analyze the final files only
+# data_filtered.index = np.arange(data_filtered.size); # Use only when filtering the data
 
-
-# Create an empty array of NaN of 990*3 (percentage,filename,camera)
-index = np.arange(990);
+#%% Create an empty array of NaN of 990*3 (percentage,filename,camera)
+index = np.arange(data_filtered.size);
 columns = ['percentage', 'filename', 'camera', 'QRcode'];
 final = pd.DataFrame(index=index, columns=columns);
 
-# Loop through the entryfile:
+
+#%% Define starting variables
 BoundingArray = np.array([4390,1518,10910,1446]);
 thresh_val = 140;
+path = 'F:\\EPPN_JRA2_201903\\Images\\';
 
-## The image path should be define accordingly ! (it is not correct here)
+#%% Create files list
+files = [f for f in glob.glob(path + "**/*.jpg", recursive=True)]
 
-#   1. Flip if needed
-for i in range(990):
+#%%   1. Flip if needed
+for i in range(data_filtered.size):
     print('Analysis: image # ' + str(i+1));
-    img_path = 'Image\Camera' + data_filtered.Camera[i] + '\\' + data_filtered.Filename[i];
+    folder = '2019-' + data_filtered.Folder[i][3:5] + '-' + data_filtered.Folder[i][:2];
+    img_path = 'F:\\EPPN_JRA2_201903\\Images\\Camera ' + data_filtered.Camera[i] + '\\' \
+        + folder + '\\' + data_filtered.Filename[i];
     flipBool = data_filtered.verticalFlip[i];
 #   2. Apply the function
     final.percentage[i] = area_percentage(img_path,thresh_val,flipBool,BoundingArray);
@@ -73,4 +77,7 @@ for i in range(990):
     
 # Export the final dataset to csv
 final.to_csv(r'final.csv','\t');
-    
+
+
+
+#%%
