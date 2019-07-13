aae893932f0c27684a7be35917d901f562825d84
Alexandre <alexandre.bohyn@gmail.com>
2019-07-13 16:49:51 +0200

    Created the skeleton of the file

---
+++ SUMMARY_PLOT.R
@@ -0,0 +1,16 @@
+# METRICS: INTRODUCTION ----
+#' Creates all the plots and tables, summarizing the metrics 
+#' for each genotype, tank, etc...
+#' Tables: mean and standard deviation for weight (D and F), AUPC, area
+#' Plots: Boxplots of the repartition between genotypes 
+#' and difference between tanks
+#' 
+#' Inputs:
+#' - Data table
+#' - Outliers table
+#'
+# USER DESIGNED FUNCTIONS -----------------------------------------------------
+
+# Load data table and design table
+load("data.RData")
+
