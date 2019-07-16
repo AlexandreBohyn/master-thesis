449cb3b26a6ce26f1a3db878506b576f1cf8ef7b
Alexandre <alexandre.bohyn@gmail.com>
2019-07-16 11:10:02 +0200

    Finishing formatting of tables and plots

--- SUMMARY_PLOT.R
+++ SUMMARY_PLOT.R
@@ -24,6 +24,34 @@ QRreformat <- function(vec){
   )
 }
 
+summ_plot <- function(var){
+  #' Creates a standard dot-plot type of plot that can be executed for 
+  #' every variables of interest
+  #' It plots the points, the weighted mean and the weighted sd as error bars
+  meanvar = paste(var, "mean", sep = "_")
+  SDvar = paste(var, "SD", sep = "_")
+  pd = position_dodge(width = 0.7)
+  ggplot(filtered_data, aes(x = factor(REAL_GENOTYPE),
+                            y = .data[[var]], color = TANK) ) +
+    geom_point(size = 1, position = pd ) +
+    geom_errorbar(data = summary_table,
+                  aes(x = factor(REAL_GENOTYPE),
+                      group = TANK,
+                      ymin = .data[[meanvar]] - .data[[SDvar]],
+                      ymax = .data[[meanvar]] + .data[[SDvar]]), 
+                  colour = "black", width = 0.1, position = pd, 
+                  inherit.aes = FALSE)+
+    geom_point(data = summary_table, 
+               aes(x=factor(REAL_GENOTYPE), y = .data[[meanvar]], fill = TANK),
+               color = "black",
+               shape = 22, size = 1.75, inherit.aes=FALSE,
+               position = pd)+
+    theme_bw() +
+    theme(legend.position = "bottom")+
+    labs(x = "GENOTYPE",
+         y = labels[[var]])
+}
+
 # Load data table and design table
 load("data.RData")
 
@@ -70,22 +98,73 @@ filtered_data <- data_table%>%
          )%>%
   select(-c(NO_RS, NO_LS, BAD_LS, NOT_FG, NO_SEED, OK, OVERLAP))
 
-# Fix the wrong NA genotypes
+# DATA FILTERING -------------------------------------------------
+
+# Fix the wrong NA genotypes and filter:
+# - outliers (null weights)
+# - null fresh/dry RS weights
+
 filtered_data <- filtered_data%>%
   mutate(REAL_GENOTYPE = ifelse(is.na(REAL_GENOTYPE) & w_WEIGHT > 0,
                                 GENOTYPE, REAL_GENOTYPE)
   )%>%
-  dplyr::filter(w_WEIGHT > 0)
+  dplyr::filter(w_WEIGHT > 0,
+                !is.na(DRY_RS),
+                DRY_RS > 0 & FRESH_LS > 0,
+                REAL_GENOTYPE != 31)
         
 
-# SUMMARY MEASURES ------------------------------------------------
+# SUMMARY TABLES ------------------------------------------------
 
-# Weighted mean
-filtered_data%>%
+# Weighted mean and SD
+summary_table <-filtered_data%>%
+  group_by(REAL_GENOTYPE,TANK)%>%
+  summarise_at(.vars = vars(DRY_LS, DRY_RS, FRESH_LS, FRESH_RS),
+               .funs = list(mean = ~wtd.mean(., weights = w_WEIGHT),
+                            SD = ~sqrt(wtd.var(., weights = w_WEIGHT, 
+                                           normwt = TRUE, na.rm=TRUE))))
+
+# Create the latex table with xtable()
+summary_latex_table <- filtered_data%>%
   group_by(REAL_GENOTYPE)%>%
   summarise_at(.vars = vars(DRY_LS, DRY_RS, FRESH_LS, FRESH_RS),
                .funs = list(mean = ~wtd.mean(., weights = w_WEIGHT),
-                            var = ~wtd.var(., weights = w_WEIGHT, 
-                                           normwt = TRUE)))%>%
-  gather(key = "key", value = "value", -REAL_GENOTYPE) -> test
-               
+                            SD = ~sqrt(wtd.var(., weights = w_WEIGHT, 
+                                               normwt = TRUE, na.rm=TRUE))))%>%
+  mutate_if(is.numeric, round, 4)%>%
+  unite(col = "FRESH_LS", starts_with("FRESH_LS"), sep = " $\\pm$ ")%>%
+  unite(col = "FRESH_RS", starts_with("FRESH_RS"),  sep = " $\\pm$ ")%>%
+  unite(col = "DRY_LS", starts_with("DRY_LS"),  sep = " $\\pm$ ")%>%
+  unite(col = "DRY_RS", starts_with("DRY_RS"),  sep = " $\\pm$ ")
+
+print(xtable(summary_latex_table, digits = rep(c(0,4), times = c(2,4)),
+             caption = "Weighted mean and standard deviation for the four
+             main variables"),
+      file = "Tables/latex _summary_table.txt",
+      append = FALSE,
+      include.rownames = FALSE,
+      caption.placement = "top",
+      sanitize.text.function = identity,
+      booktabs = TRUE,
+      timestamp = NULL,
+      comment = FALSE)
+
+  
+# SUMMARY PLOTS --------------------------------------------------
+
+# Extract the names of the vars I want to plot, the labels of the axis
+vars <- names(filtered_data[7:10])
+labels <- list(FRESH_LS = "FRESH LEAF SYSTEM WEIGHT (g)",
+             FRESH_RS = "FRESH ROOT SYSTEM WEIGHT (g)",
+             DRY_LS = "DRY LEAF SYSTEM WEIGHT (g)",
+             DRY_RS = "DRY ROOT SYSTEM WEIGHT (g)")
+
+# Stock the plots in a object
+summary_plots <- map(vars, ~summ_plot(.x))
+
+# Print them all to pdf 
+plotnames <- map(vars, ~paste0("Figures/", .x, "_summary_plot.pdf"))
+walk2(plotnames, summary_plots,
+      ~ggsave(filename = .x, plot = .y, height = 3.78, width = 7.5))
+
+      
